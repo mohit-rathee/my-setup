95ef67cc67f5549f8cc37abc7fb1baa84795295c
mohit-rathee <mohit.rathee2505@gmail.com>
2026-07-01 20:16:17 +0530

    zsh autosuggeston and syntaxhighlighting via setup

--- setup/oh-my-zsh.sh
+++ setup/oh-my-zsh.sh
@@ -7,20 +7,41 @@ fi
 
 ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 
+mkdir -p "$ZSH_CUSTOM/plugins"
+mkdir -p "$ZSH_CUSTOM/themes"
+
 clone_plugin() {
     local name="$1"
     local repo="$2"
 
-    if [[ ! -d "$ZSH_CUSTOM/plugins/$name" ]]; then
+    if [[ -d "$ZSH_CUSTOM/plugins/$name" ]]; then
+        echo "✓ $name already installed"
+    else
         echo "==> Installing $name"
         git clone --depth=1 "$repo" "$ZSH_CUSTOM/plugins/$name"
-    else
-        echo "✓ $name already installed"
     fi
 }
 
-clone_plugin "zsh-autosuggestions" \
+clone_theme() {
+    local name="$1"
+    local repo="$2"
+
+    if [[ -d "$ZSH_CUSTOM/themes/$name" ]]; then
+        echo "✓ $name already installed"
+    else
+        echo "==> Installing $name"
+        git clone --depth=1 "$repo" "$ZSH_CUSTOM/themes/$name"
+    fi
+}
+
+clone_plugin \
+    "zsh-autosuggestions" \
     "https://github.com/zsh-users/zsh-autosuggestions.git"
 
-clone_plugin "zsh-syntax-highlighting" \
+clone_plugin \
+    "zsh-syntax-highlighting" \
     "https://github.com/zsh-users/zsh-syntax-highlighting.git"
+
+clone_theme \
+    "powerlevel10k" \
+    "https://github.com/romkatv/powerlevel10k.git"
