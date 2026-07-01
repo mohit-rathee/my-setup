41740d174798f8ac08c76e0412dd03b47af3d5cf
mohit-rathee <mohit.rathee2505@gmail.com>
2026-07-01 23:36:52 +0530

    fira code font and changing shell to zsh for user

--- setup/change_shell.sh
+++ setup/change_shell.sh
@@ -8,3 +8,11 @@ if [[ -z "$ZSH_PATH" ]]; then
     echo "Error: zsh is not installed."
     exit 1
 fi
+
+if [[ "$SHELL" != "$ZSH_PATH" ]]; then
+    echo "Changing default shell to zsh..."
+    chsh -s "$ZSH_PATH"
+    echo "Default shell changed. Log out and back in for it to take effect."
+else
+    echo "zsh is already the default shell."
+fi
